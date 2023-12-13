fb01380fe6df4c79b3189bda53ed88e7d9fb7280
Ch-Ahmad0018 <chahmadmubashar@gmail.com>
2023-12-13 23:15:52 +0500

    Adding Kaun bne ga crorepati version

--- Practical/Harry Practice.py
+++ Practical/Harry Practice.py
@@ -101,4 +101,35 @@
 # else:
 #     print("Sorry no i")
 # # Else statement does not execute because loop break
-
+# -------------------------------------Kaun bne ga CrorePati game--------------------------------------------------
+questions=[["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ["Which language was used For making Facebook?","Python","JavaScript","HTML","PHP","None",4],
+           ]
+levels=[100,200,400,800,1000,1500,2000,4000,8000]
+money=0
+i=0
+for i in range(0,len(questions)):
+    question=questions[i]
+    print(f"Qustion for Rs {levels[i]}")
+    print(question[0])
+    print(f"a. {question[1]}           b. {question[2]}")
+    print(f"c. {question[3]}           d. {question[4]}")
+    reply=int(input("Enter your reply between 1 and 4: "))
+    if (reply==question[-1]):
+        print(f"Congratulation you have won money {levels[i]}")
+        if(i==4):
+            money=1000
+        elif (i==9):
+            money=4000
+    else:
+        print("Wrong Answer")
+        break
+       
+print(f"You have won total of Rs {money}")
